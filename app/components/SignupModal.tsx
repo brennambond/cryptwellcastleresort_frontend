5bfb77b76b71ae1d77e6c1e04dc57103311d1e45
Brennam Bond <brennambond@gmail.com>
2024-10-10 13:33:58 -0500

    Create user signup functionality

--- app/components/SignupModal.tsx
+++ app/components/SignupModal.tsx
@@ -1,36 +1,81 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import useSignupModal from "../hooks/useSignupModal";
 import CustomButton from "./CustomButton";
 import Modal from "./Modal";
+import apiService from "../services/apiService";
 
 const SignupModal = () => {
+  const router = useRouter();
   const signupModal = useSignupModal();
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const submitSignup = async () => {
+    const formData = {
+      email: email,
+      password1: password1,
+      password2: password2,
+    };
+
+    const response = await apiService.post(
+      "/api/auth/register/",
+      JSON.stringify(formData)
+    );
+
+    if (response.access) {
+      // handleLogin
+
+      signupModal.close();
+
+      router.push("/");
+    } else {
+      const tmpErrors: string[] = Object.values(response).map((error: any) => {
+        return error;
+      });
+      setErrors(tmpErrors);
+    }
+  };
 
   const content = (
     <>
-      <form className='space-y-4'>
+      <form action={submitSignup} className='space-y-4'>
         <input
+          onChange={(e) => setEmail(e.target.value)}
           placeholder='Your email address'
           type='email'
           className='w-full h-[54px] border border-gray-300 px-4 rounded-xl'
         />
         <input
+          onChange={(e) => setPassword1(e.target.value)}
           placeholder='Your password'
           type='password'
           className='w-full h-[54px] border border-gray-300 px-4 rounded-xl'
         />
         <input
+          onChange={(e) => setPassword2(e.target.value)}
           placeholder='Repeat password'
           type='password'
           className='w-full h-[54px] border border-gray-300 px-4 rounded-xl'
         />
 
-        <div className='p-5 bg-airbnb text-white rounded-xl opacity-80'>
-          The error message
-        </div>
+        {errors.map((error, index) => {
+          return (
+            <div
+              key={`error_${index}`}
+              className='p-5 bg-airbnb text-white rounded-xl opacity-80'
+            >
+              {error}
+            </div>
+          );
+        })}
 
-        <CustomButton label='Submit' onClick={() => console.log("Test")} />
+        <CustomButton label='Submit' onClick={submitSignup} />
       </form>
     </>
   );
--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -28,6 +28,7 @@ const apiService = {
     return new Promise((resolve, reject) => {
       fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
         method: "POST",
+        body: data,
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
