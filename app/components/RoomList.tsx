139061a6e03eb9ca08760d59da1feddace211498
Brennam Bond <brennambond@gmail.com>
2024-10-14 14:28:14 -0500

    Updates to RoomList and items

--- app/components/RoomList.tsx
+++ app/components/RoomList.tsx
@@ -1,29 +1,37 @@
 "use client";
 
-import apiService from "../services/apiService";
 import { useEffect, useState } from "react";
 import RoomListItem from "./RoomListItem";
 
 export type RoomType = {
   id: string;
   title: string;
-  image_url: string;
   price_per_night: number;
+  image_url: string;
 };
 
 const RoomList = () => {
   const [rooms, setRooms] = useState<RoomType[]>([]);
-
   const getRooms = async () => {
-    const tmpRooms = await apiService.get("/api/rooms/");
+    const url = "http://localhost:8000/api/rooms/";
 
-    setRooms(tmpRooms.data);
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("json", json);
+
+        setRooms(json.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
   };
 
   useEffect(() => {
     getRooms();
   }, []);
-
   return (
     <>
       {rooms.map((room) => {
--- app/components/RoomListItem.tsx
+++ app/components/RoomListItem.tsx
@@ -1,31 +1,27 @@
 import Image from "next/image";
 import { RoomType } from "./RoomList";
-import { useRouter } from "next/navigation";
 
 interface RoomProps {
   room: RoomType;
 }
 
 const RoomListItem: React.FC<RoomProps> = ({ room }) => {
-  const router = useRouter();
   return (
-    <div
-      onClick={() => router.push(`/rooms/${room.id}`)}
-      className='cursor-pointer'
-    >
+    <div className='cursor-pointer'>
       <div className='relative overflow-hidden aspect-square rounded-xl'>
         <Image
           fill
           src={room.image_url}
           sizes='(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px'
           className='hover:scale-110 object-cover transition h-full w-full'
-          alt='Haunted Hotel'
+          alt='Hotel Picture'
         />
       </div>
 
       <div className='mt-2'>
         <p className='text-lg font-bold'>{room.title}</p>
       </div>
+
       <div className='mt-2'>
         <p className='text-sm text-gray-500'>
           <strong>${room.price_per_night}</strong> per night
