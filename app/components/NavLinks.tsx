799ba34bc6b19216f60f37bcde9bb103e40b4e77
Brennam Bond <brennambond@gmail.com>
2024-12-01 18:35:09 -0600

    Navbar complete

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -8,7 +8,7 @@ const NavLinks = () => {
   const pathname = usePathname();
 
   return (
-    <div className='flex h-full flex-col lg:flex-row text-white-main'>
+    <div className='flex w-full lg:h-full flex-col lg:flex-row text-white-main  gap-12 lg:gap-0 pr-4 lg:pr-0 pt-4 lg:pt-0'>
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
         return (
@@ -16,8 +16,8 @@ const NavLinks = () => {
             key={link.route}
             href={link.route}
             className={`${
-              isActive && "text-[#6c53a4]  "
-            } px-[1rem] flex items-center justify-center cursor-pointer transition-colors h-full link-hover`}
+              isActive && "text-blue-main lg:text-[#6c53a4] "
+            } lg:px-[1rem] flex-center whitespace-nowrap cursor-pointer transition-colors h-full link-hover-secondary lg:link-hover`}
           >
             <span className='p-bold-18'>{link.label}</span>
           </Link>
--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -63,8 +63,8 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <SheetPrimitive.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-zinc-100 dark:ring-offset-zinc-950 dark:focus:ring-zinc-300 dark:data-[state=open]:bg-zinc-800'>
-        <Cross2Icon className='h-4 w-4' />
+      <SheetPrimitive.Close className='right-4  top-4  opacity-70  transition-opacity hover:opacity-100 focus:outline-none  f disabled:pointer-events-none data-[state=open]:bg-zinc-100 '>
+        <Cross2Icon className='h-6 w-6 p-1  text-white-main bg-gray-600 rounded-full' />
         <span className='sr-only'>Close</span>
       </SheetPrimitive.Close>
       {children}
@@ -108,7 +108,7 @@ const SheetTitle = React.forwardRef<
   <SheetPrimitive.Title
     ref={ref}
     className={cn(
-      "text-lg font-semibold text-zinc-950 dark:text-zinc-50",
+      "text-lg font-semibold text-white dark:text-zinc-50",
       className
     )}
     {...props}
