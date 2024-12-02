52d38388679150a3cf01f449cb0f794877299333
Brennam Bond <brennambond@gmail.com>
2024-12-01 20:05:44 -0600

    Create hero section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-blue-main font-cormorant'>
+      <div className='flex-center flex-col md:flex-row mini-wrapper flex-between gap-2 md:gap-4 px-5 text-center'>
+        <Link href='/'>
+          <Image
+            src='/hotel-logo-main.png'
+            alt='Haunted Hotel logo'
+            width={500}
+            height={500}
+            className='w-20 md:w-[88px] lg:w-24 px-[1.125rem] py-1 md:py-2'
+          />
+        </Link>
+        <p className='text-white-main p-bold-16 pb-4 md:pb-0'>
+          2024 Haunted Hotel. All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import CustomButton from "./components/CustomButton";
+import Image from "next/image";
+
 export default function Home() {
+  const router = useRouter();
   return (
-    <main className='max-w-[1500px] mx-auto px-6'>
-      <div className='mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6'>
-        Front Page
-      </div>
-    </main>
+    <>
+      <section className='bg-[#366f79] bg-contain py-5 md:py-10'>
+        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-10 2xl:gap-40'>
+          <div className='flex flex-col justify-center gap-8'>
+            <h1 className='h1-bold font-unifraktur text-white-main'>
+              Welcome to the Haunted Hotel
+            </h1>
+            <p className='p-regular-20 md:p-regular-24 text-gray-200 font-cormorant'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
+              sunt iusto debitis sed quis deleniti iste corporis exercitationem
+              quae! Cupiditate pariatur beatae dolore omnis!
+            </p>
+            <button
+              className='w-full rounded-full h-[54px] p-bold-20 bg-gray-800 text-white transition cursor-pointer text-center font-unifraktur tracking-wider hover:bg-opacity-70'
+              onClick={() => router.push("/rooms")}
+            >
+              Join our Covenant
+            </button>
+          </div>
+
+          <Image
+            src='/haunted-hotel-foyer.png'
+            alt='Hotel Foyer'
+            width={1000}
+            height={1000}
+            className='max-h-[70vh] object-cover object-center xl:max-w-[35vw] rounded-lg'
+          />
+        </div>
+      </section>
+    </>
   );
 }
